3d5a5a92d37254e2381cf6e5c32190a27530da28
Punkerdood <majpatmcd@gmail.com>
2020-06-16 11:01:16 -0400

    public abstract class JobField     {         public int Id { get; }         private static int nextId = 1;         public string Value { get; set; }
            public JobField()
            {
                Id = nextId;
                nextId++;
            }
    
            public JobField(string value) : this()
            {
                Value = value;
            }
    
            public override bool Equals(object obj)
            {
                return obj is JobField jobField &&
                       Id == jobField.Id;
            }
    
            public override int GetHashCode()
            {
                return HashCode.Combine(Id);
            }
    
            public override string ToString()
            {
                return Value;
            }
        }
    }

--- TechJobsOO/Job.cs
+++ TechJobsOO/Job.cs
@@ -53,39 +53,6 @@ namespace TechJobsOO
         }
     }
     // TODO: Generate Equals() and GetHashCode() methods.
-    public abstract class JobField
-    {
-        public int Id { get; }
-        private static int nextId = 1;
-        public string Value { get; set; }
-
-        public JobField()
-        {
-            Id = nextId;
-            nextId++;
-        }
-
-        public JobField(string value) : this()
-        {
-            Value = value;
-        }
-
-        public override bool Equals(object obj)
-        {
-            return obj is JobField jobField &&
-                   Id == jobField.Id;
-        }
-
-        public override int GetHashCode()
-        {
-            return HashCode.Combine(Id);
-        }
-
-        public override string ToString()
-        {
-            return Value;
-        }
-    }
-}
+  
 
 
